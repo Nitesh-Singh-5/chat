6bf716a76b1a0531824ba4ba567b1c5ea4b32c80
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-01-29 22:45:12 +0530

    deleted extra comment

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -6,7 +6,6 @@ const users = {};
 
 io.on('connection',socket =>{
     socket.on('new-user-joined',name =>{
-        // console.log("new-user",name)
         users[socket.id] = name;
         socket.broadcast.emit('user-joined',name);
     });
